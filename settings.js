45a2e89f364f31cae19815e985c70837b56fe4b9
vidhnuccx <sr4060825@gmail.com>
2025-04-19 11:45:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'STARKZ', // Your name
-  ownerNumber: '91811', //Your number
+  ownerNumber: '918113996230', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
